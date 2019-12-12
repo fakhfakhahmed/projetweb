59289d447c1f6919b21b16ffba5fe0e22f1a0e72
AhmedBelHajAmor <47744562+AhmedBelHajAmor@users.noreply.github.com>
2019-12-12 05:27:04 +0100

    Add files via upload

---
+++ CoolAdmin-master/inc/RequestC.php
@@ -0,0 +1,37 @@
+<?php
+
+include "./config.php";
+class RequestC
+{
+  
+    function afficherRequest()
+    {
+
+        $sql="select * from sisagri2.helprequest order by ID_Request asc";
+
+        $db = config::getConnexion();
+        try
+        {
+            $list=$db->query($sql);
+            return $list;
+        }
+        catch (Exception $e)
+        {
+            die('Erreur: '.$e->getMessage());
+        }
+    }
+        function supprimerRequest($ID_Request){
+            $sql="DELETE FROM helprequest where ID_Request= :ID_Request";
+            $db = config::getConnexion();
+            $req=$db->prepare($sql);
+            $req->bindValue(':ID_Request',$ID_Request);
+            try{
+                $req->execute();
+               // header('Location: index.php');
+            }
+            catch (Exception $e){
+                die('Erreur: '.$e->getMessage());
+            }
+        }
+       
+}
