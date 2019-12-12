9c0d99d7b576c8734248f54c3e922c73d52d6324
AhmedBelHajAmor <47744562+AhmedBelHajAmor@users.noreply.github.com>
2019-12-13 00:06:46 +0100

    Update SubmitRequest.php

--- farmie/SubmitRequest.php
+++ farmie/SubmitRequest.php
@@ -19,6 +19,37 @@ $PremiumMember = $_POST['PremiumMember'];
      	$stmt = $conn->prepare("insert into helprequest(FullName, Adress, Email, PhoneNumber, ProductName, PurchaseDate, PremiumMember) values(?, ?, ?, ?, ?, ?, ?)");
      	$stmt->bind_param("sssisss", $FullName, $Adress, $Email, $PhoneNumber, $ProductName, $PurchaseDate, $PremiumMember);
      	$stmt->execute();
+	    ///////////////////////////////
+      require_once(__DIR__ . '/php-rest-api-master/autoload.php');
+
+      $MessageBird = new \MessageBird\Client('xd0oL9SHbMcioJr3SXsdDjFGf'); // Set your own API access key here.
+
+      $Message             = new \MessageBird\Objects\Message();
+      $Message->originator = 'Sisagri';
+      //echo $_SESSION['tel'];
+      //$num='+'.strval( $_post['tel'] );
+      //echo '/'.$num;
+
+      $Message->recipients = array(+21697469458);
+      $Message->body       = ' Thank you dear client, a technician will contact you as fast as we can ';
+
+      try {
+          $MessageResult = $MessageBird->messages->create($Message);
+          //var_dump($MessageResult);
+          
+
+      } catch (\MessageBird\Exceptions\AuthenticateException $e) {
+          // That means that your accessKey is unknown
+          echo 'wrong login';
+
+      } catch (\MessageBird\Exceptions\BalanceException $e) {
+          // That means that you are out of credits, so do something about it.
+          echo 'no balance';
+
+      } catch (\Exception $e) {
+          echo $e->getMessage();
+      }
+      /////////////////////////////
       header("Location: HelpRequest.php?HelpOnTheWay");
      	$stmt->close();
      	$conn->close();
